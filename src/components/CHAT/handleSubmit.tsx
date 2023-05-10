3660bc74f376b974053cc9e6fcc62103abe06445
Cal <obinnaanizoba5@gmail.com>
2023-05-10 22:52:26 +0800

    fixed handlesubmit button

--- src/components/CHAT/handleSubmit.tsx
+++ src/components/CHAT/handleSubmit.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { ChatMessageContext } from "../context/chatMessageContext";
+import { useContext, useState } from "react";
+import { ChatMessageCollection, ChatMessageContext } from "../context/chatMessageContext";
 import { InputMessageContext } from "../context/inputContext";
 import { v4 as uuidv4 } from "uuid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,11 +20,7 @@ export const HandleSubmit = ({ id }: HandleSubmitProps) => {
     const { chatMessage, setChatMessage } = useContext(ChatMessageContext);
     const { input, setInput } = useContext(InputMessageContext);
     const { folder, setFolder } = useContext(FolderCollectionContext)
-
-    if (folder.length > 0) {
-        const latestMessageObject = folder[folder.length - 1].id;
-        id = latestMessageObject;
-    }
+    const [found, setFound] = useState<boolean>(false);
 
     const fetchMessage = async () => {
         if (!input) {
@@ -49,30 +45,7 @@ export const HandleSubmit = ({ id }: HandleSubmitProps) => {
             const data = await response.json();
             const newCollection = { message: input.message, response: data.choices[0].message.content, id: uuidv4() };
             setChatMessage([...chatMessage, newCollection]);
-            console.log(id);
-            let newMessageId = uuidv4()
-            // setFolder([...folder, { id: uuidv4(), collection: [{ message: input.message, response: data.choices[0].message.content, id: newMessageId }] }]);
-
-            setFolder((folder)=> folder.map((collections)=> collections.id === id ?{...collections, id: newMessageId, collection:[...collections.collection, {message:input.message, response: data.choices[0].message.content, id: newMessageId }]}:collections));
-            // setFolder((folder) => folder.map((collections) => collections.id === id ? { ...collections, id: newMessageId, collection: [...collections.collection, { message: input.message, response: data.choices[0].message.content, id: uuidv4() }] } : collections));
-
-            // const newCollection = { message: input.message, response: data.choices[0].message.content, id: uuidv4() };
-            // setChatMessage([...chatMessage, newCollection]);
-            // console.log("current-id", id);
-
-            // let collectionExists = false;
-            // setFolder(folder.map((collections) => {
-            //   if (collections.id === id) {
-            //     collectionExists = true;
-            //     return { ...collections, collection: [...collections.collection, newCollection] };
-            //   } else {
-            //     return collections;
-            //   }
-            // }));
-
-            // if (!collectionExists) {
-            //   setFolder([...folder, { id: uuidv4(), collection: [newCollection] }]);
-            // }
+            addNewChat(newCollection)
             setInput({ message: "", id: uuidv4() });
             setChatMessage([]);
 
@@ -83,6 +56,15 @@ export const HandleSubmit = ({ id }: HandleSubmitProps) => {
 
     const sendMessage = () => {
         fetchMessage();
+    }
+
+    const addNewChat  = (newCollection:ChatMessageCollection) =>{
+        const index = folder.findIndex((item)=> item.id === id);
+        if(index === -1){
+            setFolder((folder)=> [...folder, {id: uuidv4(), collection: [newCollection]}]);
+        }else{
+            setFolder((folder)=> folder.map((collections)=> collections.id === id ? {...collections, collection:[...collections.collection, newCollection]}:collections));
+        }
     }
 
     return (
