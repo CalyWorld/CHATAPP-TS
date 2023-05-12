b9ad648ef74e86ad6d6cfa79c47b41b55628fa2a
Cal <obinnaanizoba5@gmail.com>
2023-05-12 22:25:38 +0800

    added delay value to show sign of getting response in chats

--- src/components/CHAT/card.tsx
+++ src/components/CHAT/card.tsx
@@ -11,15 +11,17 @@ export type CardProps = {
 
 export const Card = ({ collections, id }: CardProps) => {
 
+    const currentId = collections[collections.length-1].id;
+
     return (
         <div id="chat-container" className="w-screen h-screen bg-backgroundColor text-textColor flex flex-col justify-between overflow-y-scroll">
             <div className="flex flex-col">
                 {collections.map((collection) =>
-                    <MiniCard key={collection.id} collection={collection}/>
+                    <MiniCard key={collection.id} collection={collection} currentId={currentId}/>
                 )}
             </div>
             <div className="w-full flex flex-col items-center mb-20 gap-10">
-                <div id="chat-message-container" className="flex justify-center item-center w-96">
+                <div id="chat-message-container" className="flex justify-center gap-3 item-center w-96">
                     <HandleChange />
                     <HandleSubmit id={id} />
                 </div>
