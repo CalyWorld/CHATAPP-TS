673b15172c8d307c2bae142cdd6b515994d662dd
Cal <obinnaanizoba5@gmail.com>
2023-05-14 16:14:55 +0800

    added delete functionality to clear all chats

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -1,30 +1,35 @@
 import { SignOut } from "../auth/signOut";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faGear, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { FolderCollectionContext } from "../context/folderCollectionContext";
 
 interface DropDownProps {
     setDropDown: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const DropDown = ({setDropDown}:DropDownProps) => {
+export const DropDown = ({ setDropDown }: DropDownProps) => {
+
+    const { setFolder } = useContext(FolderCollectionContext)
 
     return (
-        <div id="dropdown" className="absolute p-1 bottom-12 left-4 h-64 w-60 flex justify-start items-center bg-dropdownBackground text-dropdownText rounded-lg shadow w-44 dark:bg-gray-700" >
+        <div id="dropdown" className="absolute p-1 bottom-12 left-3 h-64 w-60 flex justify-start items-center bg-dropdownBackground text-dropdownText rounded-lg shadow w-44 dark:bg-gray-700" >
             <ul className="flex flex-col gap-3 w-full">
                 <li className="back-button flex justify-end">
-                    <button onClick={()=>{setDropDown(false)}}><FontAwesomeIcon icon={faXmark} size="lg" /></button>
+                    <button onClick={() => { setDropDown(false) }}><FontAwesomeIcon icon={faXmark} size="lg" /></button>
                 </li>
                 <li className="flex justify-start p-2 w-full hover:bg-dropdownTextBackground">
                     <div className="flex items-center gap-3">
                         <p>Helpn & FAQ</p>
                     </div>
                 </li>
-                <li className="flex justify-start p-2 bg w-full hover:bg-dropdownTextBackground">
+                <li className="flex justify-start p-2 bg w-full hover:bg-dropdownTextBackground" >
                     <div className="flex items-center gap-3"><FontAwesomeIcon icon={faGear} />
                         <p>Settings</p></div>
                 </li>
                 <li className="flex justify-start p-2 w-full hover:bg-dropdownTextBackground">
-                    <div className="flex items-center gap-3"><FontAwesomeIcon icon={faTrash} />
+                    <div className="flex items-center gap-3" onClick={() => { setFolder([]) }}>
+                        <FontAwesomeIcon icon={faTrash} />
                         <p>Clear conversations</p>
                     </div>
                 </li>
