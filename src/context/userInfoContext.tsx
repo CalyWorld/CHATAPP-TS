144ea6571eb58142344f6e5d26957779cebc0ef0
Cal <obinnaanizoba5@gmail.com>
2023-05-14 13:08:30 +0800

    changed the state and setter variables to be type null

--- src/context/userInfoContext.tsx
+++ src/context/userInfoContext.tsx
@@ -1,21 +1,17 @@
 import { createContext } from "react"
 
 export interface UserInfoCollection {
-    email: string | null;
-    img: string | null;
+    email?: string | null;
+    img?: string | null;
     id: string;
 }
 
 export interface userContextType {
-    user: UserInfoCollection,
-    setUser: React.Dispatch<React.SetStateAction<UserInfoCollection>>
+    user: UserInfoCollection | null,
+    setUser: React.Dispatch<React.SetStateAction<UserInfoCollection | null>>
 }
 
 export const UserInfoContext = createContext<userContextType>({
-    user: {
-        email: "",
-        img: "",
-        id: ""
-    },
+    user: null,
     setUser: () => { }
 })
